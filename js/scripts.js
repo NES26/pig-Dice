e1cb6c093941aa05c128d040b8d918491faf2e7c
nirnuhsimel@gmail.com <nirnuhsimel@gmail.com>
2019-04-07 22:22:11 +0300

    added content in index.html

---
+++ js/scripts.js
@@ -0,0 +1,12 @@
+ var randomNumbaer = Math.floor(math.random() * 6) // 1 - 6
+var playerOneImages = "images/"+randomNumber+".png"
+
+document.querySelector('img1').setAttribute('src'. playerOneImages):
+
+if (playerOneImages > playerOneImages1) {
+    document.querySelector('h1').innerHTML = 'player 1 Win';
+} else if (playerOneImages < playerOneImages) {
+    document.querySelector(h1).innerHTML = 'playe 2 Win';
+} else {
+    document.querySelector(h1).innerHTML = 'Roll Again';
+}
