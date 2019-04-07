c8ee292c95a6b14bd0cccdd3df13a5b3811fdffe
nirnuhsimel@gmail.com <nirnuhsimel@gmail.com>
2019-04-07 23:42:58 +0300

    Added content in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -24,3 +24,50 @@ function activeUser() {
         $('.player2Area').removeClass('disableGamingArea');
     } 
 
+};
+//Funtion on what is to happen when the dice is rolled.
+Player.prototype.roll = function () {
+    var randomNo = Math.floor((Math.random() * 6) + 1); //Random no generator from 1-6.
+    this.diceRoll = randomNo;
+    activeUser();
+    if (randomNo === 1) {
+        this.turnTotal = 0;
+        this.diceRoll = 1;
+        if (this.active === player1.active) { //disable and enable gaming areas when dice is a 1 according to which player is active.
+            player1.active = false;
+            player2.active = true;
+            $('.player1Area').children().prop('disabled', true);
+            $('.player1Area').addClass('disableGamingArea');
+            $('.player2Area').children().prop('disabled', false);
+            $('.player2Area').removeClass('disableGamingArea');
+        } else if (this.active === player2.active) {
+            player2.active = false;
+            player1.active = true;
+            $('.player2Area').children().prop('disabled', true);
+            $('.player2Area').addClass('disableGamingArea');
+            $('.player1Area').children().prop('disabled', false);
+            $('.player1Area').removeClass('disableGamingArea');
+        } else {
+            console.log("not working");
+        }
+        return alert("Oops you got a 1. Your turn is over!");
+    } else {
+        this.turnTotal += randomNo;
+    };
+    return this.diceRoll;
+};
+//Function on what is to happen when a player holds the game.
+Player.prototype.hold = function () {
+    activeUser();
+    this.overallScore += this.turnTotal;
+    if (this.overallScore >= 100) {
+        alert("Game Over. You win!!!!");
+        resetFields();
+        alert('To play with a new partner click New Game.')
+
+    } else {
+        return false;
+    }
+    console.log('the turn total is: ' + this.turnTotal);
+    return this.overallScore;
+};
