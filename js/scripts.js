e355880b57d0e1eee43008a8652d0564c19bce18
nirnuhsimel@gmail.com <nirnuhsimel@gmail.com>
2019-04-07 23:34:58 +0300

    added styles in styles.css

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
- var randomNumbaer = Math.floor(math.random() * 6) // 1 - 6
-var playerOneImages = "images/"+randomNumber+".png"
+ var randomNumbaer = Math.floor(math.random() * 6) + 1 // 1 - 6
+var playerOneImages = "img/Dice"+randomNumber;
+var randomNumbaer1 = Math.floor(math.random() * 6) + 1 // 1 - 6
+var playerOneImages1 = "img/Dice"+randomNumber1;
 
-document.querySelector('img1').setAttribute('src'. playerOneImages):
 
-if (playerOneImages > playerOneImages1) {
+document.querySelector('.img1').setAttribute('src'. playerOneImg);
+document.querySelector('.img2').setAttribute('src'. playerOneImg);
+
+
+if (playerOneImages > playerOneImg1) {
     document.querySelector('h1').innerHTML = 'player 1 Win';
-} else if (playerOneImages < playerOneImages) {
+} else if (playerOneImg < playerOneImg) {
     document.querySelector(h1).innerHTML = 'playe 2 Win';
 } else {
     document.querySelector(h1).innerHTML = 'Roll Again';
